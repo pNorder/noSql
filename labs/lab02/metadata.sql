e56c67e304c7b4bdc50411770a8d206cdf0d19cd
Trace Levinson <trace.levinson@du.edu>
2019-04-08 13:09:39 -0600

    Update to Lab 2 code
    Added default true to alter table command
    changed pg_class from oid to relname query

--- labs/lab02/metadata.sql
+++ labs/lab02/metadata.sql
@@ -1,6 +1,6 @@
 SELECT * 
 FROM pg_class P
-WHERE P.oid = 16395 OR 
-P.oid = 16385 OR
-P.oid = 16411 OR
-P.oid = 16429;
+WHERE P.relname = 'cities' OR 
+P.relname = 'countries' OR 
+P.relname = 'events' OR 
+P.relname = 'venues';

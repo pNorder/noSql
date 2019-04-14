3c7a93a46608fad3fbc509660f08de72b3c4624d
Trace Levinson <trace.levinson@du.edu>
2019-04-14 17:26:22 -0600

    Finish 6-1

---
+++ labs/lab03/create_delete_rule.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE FUNCTION set_inactive() RETURNS trigger AS $$
+    DECLARE
+    BEGIN
+        UPDATE venues SET active = false WHERE venues.venue_id = OLD.venue_id;
+        RAISE NOTICE 'MARKED VENUE WITH ID #% AS INACTIVE', OLD.venue_id;
+        RETURN NULL;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_inactive 
+    BEFORE DELETE ON venues
+    FOR EACH ROW EXECUTE PROCEDURE set_inactive();
+
+        RAISE NOTICE 'MARKED VENUE WITH ID #% AS INACTIVE', OLD.venue_id;

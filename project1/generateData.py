a4841525837f839437b43b5a9b49ed9ed620297f
Trace Levinson <trace.levinson@du.edu>
2019-05-19 13:38:50 -0600

    Mongo user data

--- project1/generateData.py
+++ project1/generateData.py
@@ -5,6 +5,7 @@ def generateUsers():
     fake = Faker()
     fake.random.seed(12345)
     userFile = open("users.csv", "w")
+    userMongo = open("usersMongo.js", "w")
     for i in range(0,5000):
         user_id = i
         email = fake.email()
@@ -18,12 +19,20 @@ def generateUsers():
         state = fake.state_abbr()
         userFile.write(str(user_id) + ',' + email + ',' + firstName + ',' + surname + ',' + str(phone) +
                        ',' + street + ",NULL,"  + city + ',' + state + ',' + zipcode + '\n')
-
+        
+        userMongo.write("db.users.insert({emailAddress:\"" + email + "\",name: { first: \"" + firstName + "\", last:\"" + surname + "\"}, phoneNumber: \"" + str(phone) + "\", address: { line1: \"" + street + "\"},city: \"" + city + "\", state: \"" + state + "\", zip: \"" + zipcode + "\"})\n") 
+                        
+                
 def generateRecipes():
         recipeFile = open("recipes.csv", "w")
         for i in range(0, 50):
-                recipeFile.write(str(i) + ',' + "recipe" + str(i) + ',' + "description " + str(i) + ',' + "1000" 
-                      + "\n")
+                recipeFile.write(str(i) + ',' + "recipe" + str(i) + ',' 
+                                 + "description " + str(i) + ',' + 
+                                 "Instructions " + str(i) + "\n")
+                
+def generateIngredients():
+        ingred = ["mozzarella", "parmesean", "ricota", "gorgonzola", "flour", "salt", "olive oil",
+                   ""]
 
 def generateOrders():
     fake = Faker()
@@ -34,6 +43,6 @@ def generateOrders():
         datetime = fake.date() +" " + fake.time()
         orderFile.write(str(user) + ',' + datetime + ',' + str(recipe) + '\n')
         
-#generateUsers()
-generateRecipes()
-generateOrders()
+generateUsers()
+#generateRecipes()
+#generateOrders()

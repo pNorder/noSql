b5dca1978fde8ba5c1371cf138734b9a5154d717
Trace Levinson <trace.levinson@du.edu>
2019-05-22 11:22:17 -0600

    Updates for add order
    now it will work even with ingredients used more than 1
    prints name of limiting ingredient

--- project1/postgres/addOrder.sql
+++ project1/postgres/addOrder.sql
@@ -1,10 +1,4 @@
-
-CREATE OR REPLACE RULE update_inventory AS ON INSERT TO Orders DO ALSO
-(Update Ingredients SET quantity = quantity - amount FROM CookedWith
-WHERE new.recipe_id = CookedWith.recipe_id AND Ingredients.ingredient_id = CookedWith.ingredient_id);
-
 DROP FUNCTION add_order(integer, integer);
-
 CREATE OR REPLACE FUNCTION add_order(
     user_id_param integer,
     recipe_id_param integer)
@@ -15,16 +9,29 @@ DECLARE
     startcount integer;
     endcount integer;
     inserted boolean:= false;
+    quantneeded integer;
+    newquant integer;
 BEGIN
     SELECT COUNT(*) INTO startcount FROM Orders O WHERE O.user_id = user_id_param;
+    --This loop checks to see if there is necessary quantity of ingredients
     FOR temprow IN
         SELECT * FROM Ingredients I, cookedwith CW WHERE  CW.recipe_id = recipe_id_param AND I.ingredient_id = CW.ingredient_id
         LOOP 
-            IF temprow.quantity <= 0 THEN
-                RAISE NOTICE 'Not Sufficient Quantity for Order';
+            SELECT CW.quantity INTO quantneeded FROM cookedwith CW WHERE CW.recipe_id = recipe_id_param AND temprow.ingredient_id = CW.ingredient_id;
+            IF temprow.quantity - quantneeded <= 0 THEN
+                RAISE NOTICE 'Not Sufficient Quantity of % for Order', temprow.name;
                 inserted:=false;
                 RETURN inserted;            
             END IF;
+        END LOOP;
+
+    --This loop changes update the ingredients
+    FOR temprow IN
+        SELECT * FROM Ingredients I, cookedwith CW WHERE  CW.recipe_id = recipe_id_param AND I.ingredient_id = CW.ingredient_id
+        LOOP 
+            SELECT CW.quantity INTO quantneeded FROM cookedwith CW WHERE CW.recipe_id = recipe_id_param AND temprow.ingredient_id = CW.ingredient_id;
+            newquant:= temprow.quantity - quantneeded;
+            UPDATE Ingredients SET quantity = newquant WHERE ingredient_id = temprow.ingredient_id;
         END LOOP;
     INSERT INTO Orders(user_id, time, recipe_id) 
         VALUES (user_id_param, clock_timestamp(), recipe_id_param);
